22c059e952a5f365efe68ae9f368d4fbb6a46936
Greg Brown <gregplaysguitar@gmail.com>
2013-05-14 15:23:52 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+jquery.formstyles.js
+====================
+
+Generates masks for form elements to facilitate consistent cross-browser
+styling. Currently works with selects, and checkbox, radio and submit 
+inputs.
+
+
+Example use
+-----------
+
+    $(function() {
+        $("select").selectstyles();
+        $('input[type="checkbox"], input[type="radio"]').checkboxstyles();
+        $('input[type="submit"]').buttonstyles();
+    });
+
+
+Example select styles
+---------------------
+
+    .select-mask {
+        display: inline-block;
+        border-bottom: 2px $black solid;
+        width: 230px;
+    }
+    .select-mask select {
+        cursor: pointer;
+    }
+    .select-mask span {
+        display: block;
+        padding: 5px 0;
+    }
+    .select-mask span:after {
+        content: '↓';
+        float: right;
+    }
---
+++ jquery.formstyles.js
@@ -0,0 +1,146 @@
+(function($) {
+
+    /*
+
+    Example use:
+
+        $("select").selectstyles();
+        $('input[type="checkbox"], input[type="radio"]').checkboxstyles();
+        $('input[type="submit"]').buttonstyles();
+
+    TODO:
+
+    ✔ Wrap selects in a mask 
+    ✔ Wrap checkbox/radio
+    ✔ Wrap submit buttons
+    ✔ select to 0.01 opacity and fill mask
+    ✔ customisable callback to set text in mask on change
+    - expose an api to update the content, maybe? or not needed?
+    ✔ handle focused states
+    - handle disabled states
+    - ellipsis for text that's too long? Or do we rely on css
+
+    */
+
+
+    COMMON_CSS = {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: '0',
+        height: '100%',
+        width: '100%',
+        opacity: 0.01,
+        background: 'none',
+        border: 'none'
+    };
+
+
+    function createmask(el) {
+        var mask = $('<div>');
+        mask.css({
+            position: 'relative'
+        });
+        mask.insertBefore(el).append(el);
+        handle_focus(el, mask);
+        return mask;
+    };
+
+    function handle_focus(el, mask) {
+        el.on('focus', function() {
+            mask.addClass('focused');
+        });
+        el.on('blur', function() {
+            mask.removeClass('focused');
+        });
+    };
+
+    $.fn.selectstyles = (function(options) {
+        var options = $.extend({
+                display_val: function(val, select) {
+                    return val;
+                },
+                classname: 'select-mask',
+            }, options);
+
+        this.each(function() {
+            var select = $(this),
+                mask = createmask(select).addClass(options.classname),
+                label = $('<span>');
+
+            mask.css('minWidth', select.width());
+            mask.append(label);
+
+            select.css(COMMON_CSS);
+
+            function set_val() {
+                var val = select.find('option:selected').text();
+                label.text(options.display_val(val, select));
+            };
+            set_val();
+            select.on('change', set_val);
+
+            // needed to set the width in ie 
+            select[0].style.width = mask.width();
+            select[0].style.height = mask.outerHeight();
+        });
+
+        return this;
+    });
+
+    $.fn.checkboxstyles = (function(options) {
+        var options = $.extend({
+                classname: 'checkbox-mask',
+            }, options);
+
+        this.each(function() {
+            var input = $(this),
+                mask = createmask(input).addClass(options.classname);
+
+            input.css(COMMON_CSS).css({
+                width: mask.outerWidth(),
+                height: mask.outerHeight()
+            });
+
+            function set_val() {
+                if (input.is(':checked')) {
+                    mask.addClass('checked');
+                }
+                else {
+                    mask.removeClass('checked');
+                }
+            };
+            set_val();
+            input.on('change click', set_val);
+        });
+
+        return this;
+    });
+
+    BUTTON_CSS = {
+        position: 'absolute',
+        top: 0,
+        left: '0',
+        opacity: 0.01
+    };
+
+    $.fn.buttonstyles = (function(options) {
+        var options = $.extend({
+                classname: 'button-mask',
+            }, options);
+
+        this.each(function() {
+            var input = $(this),
+                mask = createmask(input).addClass(options.classname);
+
+            input.css(COMMON_CSS);
+            mask.append(input.val());
+        });
+
+        return this;
+    });
+
+
+})(jQuery);
+
